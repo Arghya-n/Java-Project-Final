8cd271dce7004170e686e3c503ce7fb37193192b
Arghya-n <sakib.hb7@gmail.com>
2023-03-27 22:11:54 +0600

    New commit

---
+++ dist/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"vote","l":"Candidates"},{"p":"vote","l":"Dashboard"},{"p":"vote","l":"ElectionList"},{"p":"vote","l":"Elections"},{"p":"vote","l":"Login"},{"p":"vote","l":"MainMenu"},{"p":"vote","l":"Vote"},{"p":"vote","l":"Voters"},{"p":"vote","l":"Voting"}];updateSearchResults();
